03ffe74d25493e9d967d5e57fa1c9478f581c79a
Dawn Olivier <dmarieolivier@gmail.com>
2022-05-21 15:46:51 -0400

    commit tree 63977ca4500377ee28fbc8a72c368f6a3e344225

--- main.js
+++ main.js
@@ -1,8 +1,69 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errorModal = document.querySelector("#modal")
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  
+  // Add the .hidden class to the error modal in the HTML so it does not appear when the page first loads
+
+  errorModal.classList.add("hidden")
+})
+
+// call find likes
+// findLikes()
+clickListener()
+
+function hideError(){
+  errorModal.classList.add("hidden")
+}
+
+  function findLikes(){
+   const likeArr = document.querySelectorAll(".like-glyph")
+  
+  likeArr.forEach((singularLike) => {
+    singularLike.addEventListener("click", () => console.log("LIKE YOU FOUND ME!"))
+  })
+}
+
+  function clickListener() {
+    document.addEventListener('click', (event) => {
+      if (event.target.classList[0] === 'like-glyph'){
+
+
+        mimicServerCall()
+          .then((resp) => {
+            const activated = event.target.classList.contains("activated-heart");
+            if(activated){
+              event.target.classList.remove("activated-heart")
+              event.target.innerHTML = EMPTY_HEART
+            } else {
+              event.target.classList.add("activated-heart");
+              event.target.innerHTML = FULL_HEART;
+            }
+
+            activated;
+          }) //.300ms
+          .catch((error) => {
+            console.log(error);
+            errorModal.remove("hidden");
+            setTimeout(() => {
+            hideError();
+          }, 3000)
+        });
+      }
+    });
+  }
+    
+  // When a user clicks on an empty heart:
+  //      * Invoke `mimicServerCall` to simulate making a server request
+
+
+
+
+
+
 
 
 
